786f012f06cf4dbb25d8823273e5101cac1e6347
Odran HUSSON <odran@webforger.fr>
2021-04-10 16:53:46 +0200

    Trading goal model

--- app/Models/tradingGoal.php
+++ app/Models/tradingGoal.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\tradingPool;
 
 class tradingGoal extends Model
 {
     use HasFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function tradingPool() {
+        return $this->hasOne(tradingPool::class);
+    }
 }
