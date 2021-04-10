206642c9ed09e5ce22570e4ac52b905f7e9b4ba6
Odran HUSSON <odran@webforger.fr>
2021-04-10 13:40:34 +0200

    Add many to many relationship in tradingPool migratoin

--- database/migrations/2021_04_10_101702_init_project_database.php
+++ database/migrations/2021_04_10_101702_init_project_database.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class InitProjectDatabase extends Migration
 {
     CONST DB_TRADING_POOLS_NAME = 'trading_pools';
+    CONST DB_TRADING_POOLS_USERS_NAME = 'trading_pool_user';
     /**
      * Run the migrations.
      *
@@ -16,12 +17,19 @@ class InitProjectDatabase extends Migration
     {
         Schema::create($this::DB_TRADING_POOLS_NAME, function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->text('name');
             $table->timestamps();
         });
 
-        Schema::table($this::DB_TRADING_POOLS_NAME, function (Blueprint $table) {
+        Schema::create($this::DB_TRADING_POOLS_USERS_NAME, function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('trading_pool_id');
+        });
+
+        Schema::table($this::DB_TRADING_POOLS_USERS_NAME, function (Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('trading_pool_id')->references('id')->on('trading_pools');
         });
     }
 
@@ -32,6 +40,14 @@ class InitProjectDatabase extends Migration
      */
     public function down()
     {
+        Schema::table($this::DB_TRADING_POOLS_USERS_NAME, function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['trading_pool_id']);
+            $table->dropColumn('user_id');
+            $table->dropColumn('trading_pool_id');
+        });
+
+        Schema::dropIfExists($this::DB_TRADING_POOLS_USERS_NAME);
         Schema::dropIfExists($this::DB_TRADING_POOLS_NAME);
     }
 }
---
+++ database/seeders/tradingPool.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class tradingPool extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
