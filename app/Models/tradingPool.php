f2899c2175cec15415ce8cf57115a11a3f5ee7a9
Odran HUSSON <odran@webforger.fr>
2021-04-10 13:40:51 +0200

    Add many to many relationship in tradingPool and user

--- app/Models/tradingPool.php
+++ app/Models/tradingPool.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use app\Models\User;
 
 class tradingPool extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
