dd79e40f7806a32aab6ccd7814b355f2ec08e8fe
Odran HUSSON <odran@webforger.fr>
2021-04-11 13:35:08 +0200

    Sb2 vendors

--- resources/views/poc/layout.blade.php
+++ resources/views/poc/layout.blade.php
@@ -12,7 +12,7 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/poc/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -738,17 +738,17 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap//poc/js/bootstrap.bundle.min.js"></script>
+    <script src="/poc/vendor/jquery/jquery.min.js"></script>
+    <script src="/poc/vendor/bootstrap//poc/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/poc/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
     <script src="/poc/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/chart./poc/js/Chart.min.js"></script>
+    <script src="/poc/vendor/chart./poc/js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="/poc/js/demo/chart-area-demo.js"></script>
