122396a24707ba5330a6c674141c9bd28134ccc9
Odran HUSSON <odran@webforger.fr>
2021-04-12 15:32:33 +0200

    Cypress tests for home page login and admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,6 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
