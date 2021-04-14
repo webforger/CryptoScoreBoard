8f1084fad3756bb606ae7723cb8ccae3a98b7d00
Odran HUSSON <odran@webforger.fr>
2021-04-14 21:10:03 +0200

    Navbar burger menu wip

--- resources/views/static.blade.php
+++ resources/views/static.blade.php
@@ -6,10 +6,29 @@
     <meta name="csr-token" content="{{ csrf_token() }}">
     <title>Trading Wars</title>
     <link href="{{ asset('/frontapp/app.css') }}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 </head>
 <body>
     <nav id="nav-bar">
-        <img src="/frontapp/img/logo.svg" alt="logo">
+        <input id="menu__toggle" type="checkbox" />
+        <label class="menu__btn" for="menu__toggle">
+            <span></span>
+        </label>
+        <ul class="menu__box">
+            <li>
+                <img src="/frontapp/img/logo.svg" alt="logo">
+            </li>
+            <li>
+                <a class="menu__item" href="#">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a class="menu__item" href="#">
+                    Menu item 2
+                </a>
+            </li>
+        </ul>
     </nav>
     <div id="container">
         <div class="grid">
