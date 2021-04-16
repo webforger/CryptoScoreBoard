1c801cd85ecfc22ee84073d321471b5d7a2f6c0a
Odran HUSSON <odran@webforger.fr>
2021-04-16 12:05:19 +0200

    Improve cypress testing

--- tests/cypress/integration/login.js
+++ tests/cypress/integration/login.js
@@ -1,9 +1,11 @@
 describe('login', () => {
     it('Login as admin', () => {
         cy.visit('/login');
+        cy.intercept('/login', []).as('login')
         cy.get('input[type=email]').type(Cypress.env('adminEmail'));
         cy.get('input[type=password]').type(Cypress.env('adminPassword'));
         cy.get('button[type=submit]').click();
+        cy.wait('@login');
         cy.getCookie('cryptoscoreboard_session').should('exist')
     })
 })
--- tests/cypress/support/login.js
+++ tests/cypress/support/login.js
@@ -3,26 +3,26 @@ Cypress.Commands.add("login", (email = null, password = null) => {
     cy.request({
         method: 'GET',
         url: '/sanctum/csrf-cookie'
-    })
-
-    email = email || Cypress.env('defaultEmail')
-    password = password || Cypress.env('defaultPassword')
-    let _token
-    cy.visit('/login')
-        .getCookie('XSRF-TOKEN')
-        .then((cookie) => {
-            cy.request({
-                method: 'POST',
-                url: '/login',
-                form: true,
-                body: {
-                    email,
-                    password
-                },
-                headers: {
-                    // Base64 encoded string was URI encoded in headers. Decode it.
-                    'X-XSRF-TOKEN': decodeURIComponent(cookie.value)
-                }
+    }).then(() => {
+        email = email || Cypress.env('defaultEmail')
+        password = password || Cypress.env('defaultPassword')
+        let _token
+        cy.visit('/login')
+            .getCookie('XSRF-TOKEN')
+            .then((cookie) => {
+                cy.request({
+                    method: 'POST',
+                    url: '/login',
+                    form: true,
+                    body: {
+                        email,
+                        password
+                    },
+                    headers: {
+                        // Base64 encoded string was URI encoded in headers. Decode it.
+                        'X-XSRF-TOKEN': decodeURIComponent(cookie.value)
+                    }
+                })
             })
-        })
+    })
 })
